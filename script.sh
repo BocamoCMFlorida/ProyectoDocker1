a11afb82f95f808f05f69308d8296dc67c52307f
BocamoCMFlorida <bocamo@alumnatflorida.es>
2025-03-24 10:08:25 +0100

    funcion crear imagen

--- script.sh
+++ script.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+#Nombrar variables
+Nombre_Imagen="mi-nginx"
+Nombre_Container="mi-nginx-container"
+#Funcion para crear imagen
+function crear_imagen(){
+    echo "Creando imagen"
+    docker build -t $Nombre_Imagen .
+    echo "Imagen creada"
+}
