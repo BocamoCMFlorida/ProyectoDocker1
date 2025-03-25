74589bceeb7d2f5162154f7360e5e16af01727e9
BocamoCMFlorida <bocamo@alumnatflorida.es>
2025-03-25 11:54:07 +0100

    funcion ejecutar contenedor

--- script.sh
+++ script.sh
@@ -8,3 +8,8 @@ function crear_imagen(){
     docker build -t $Nombre_Imagen .
     echo "Imagen creada"
 }
+ejecutar_contenedor(){
+    echo "Ejecutando contenedor"
+    docker run -d --name $CONTAINER_NAME -p 8080:80 $IMAGE_NAME
+    echo "Contenedor ejecutándose en http://localhost:8080"
+}
