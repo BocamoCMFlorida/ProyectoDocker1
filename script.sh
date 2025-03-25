1f9a38163b338315896a5b2c5a24308dc76c3799
BocamoCMFlorida <bocamo@alumnatflorida.es>
2025-03-25 11:55:59 +0100

    parar contenedor

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 Nombre_Imagen="mi-nginx"
 Nombre_Container="mi-nginx-container"
 #Funcion para crear imagen
-function crear_imagen(){
+crear_imagen(){
     echo "Creando imagen"
     docker build -t $Nombre_Imagen .
     echo "Imagen creada"
@@ -12,4 +12,10 @@ ejecutar_contenedor(){
     echo "Ejecutando contenedor"
     docker run -d --name $CONTAINER_NAME -p 8080:80 $IMAGE_NAME
     echo "Contenedor ejecutándose en http://localhost:8080"
+}
+parar_contenedor(){
+    echo "Parando contenedor"
+    docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
+    echo "Contenedor parado y eliminado"
 }
